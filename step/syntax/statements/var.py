3a95291a490e4200c6a9b47c6a13f0ebe7432116
Ayman <ayman.alshanqiti@gmail.com>
2019-12-05 19:30:31 +0300

    EoS

--- step/syntax/statements/var.py
+++ step/syntax/statements/var.py
@@ -2,22 +2,26 @@ from step.lex.token import Token, EOFToken
 from step.syntax.types import *
 from step.syntax.handler import *
 
-# var datatype id [= expression]
+# var | let datatype id [= expression]
 class VarStatement(Statement):
   def __init__(self, token=None, datatype=None, identifier=None, expression=None, level=0, parent_stmt=None, next_stmt=None,previous_stmt=None):
     super().__init__(token, level, parent_stmt, next_stmt,previous_stmt)
     self.datatype = datatype
     self.identifier = identifier
     self.expression = expression
+    self.is_const = False
 
 
 class VarStatementParser(ParserHandler):
   def is_parsable(self, parser):
     token = parser.token
-    return token.category == 'keyword' and token.value == 'var'
+    return token.category == 'keyword' and (token.value == 'var' or token.value == 'let')
 
   def parse(self, parser, parent=None):
     statement = VarStatement(parser.token, None, None, None, parser.statement_level, parent)
+    if parser.token.value == 'let':
+      statement.is_const = True
+      
     parser.expect('keyword', 'keyword')
     if not parser.token.value in ['int', 'float', 'string', 'boolean']:
       parser.syntax_error()
